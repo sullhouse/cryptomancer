b24a03ea44c8cc20c66a497c7a9d4616ca137298
Andrew Sullivan <sullhouse@gmail.com>
2017-06-17 23:15:55 -0400

    Adding settings to be able to run main jar and check args

--- src/main/java/com/sullbrothers/crypto/app/App.java
+++ src/main/java/com/sullbrothers/crypto/app/App.java
@@ -1,13 +1,34 @@
 package com.sullbrothers.crypto.app;
 
-/**
- * Hello world!
- *
- */
+import java.sql.SQLException;
+
+import com.sullbrothers.crypto.coinbase.*;
+import com.sullbrothers.crypto.database.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Running main application." );
+        for(int i = 0; i < args.length; i++) {
+            String arg = args[i].toLowerCase();
+            switch (arg) {
+                case "getrates":
+                    try {
+                        getLatestExchangeRates("BTC");
+                    } catch (Exception e) {
+                        //TODO: handle exception
+                    }
+                    break;
+                default:
+                    System.out.println( "Unrecognized argument: " + arg );
+                    break;
+            }
+        }
+    }
+
+    private static void getLatestExchangeRates(String currency) throws SQLException {
+        ExchangeRates exchangeRates = new ExchangeRates(currency);
+        new RateHistoryDAO(exchangeRates.getTimestamp(), exchangeRates.getExchageRates());
     }
 }
