2df28308a6bb8cb34e47963364aef6b90ec31f6d
NUMERIX\asulliva <asulliva@numerix.com>
2017-06-25 23:21:45 -0400

    Added classifications to our training data.  Next step is to start drawing in more pieces of meaningful data I think I've mathematically proven that our current wallet is completely irrelevant btw, so we don't need to worry about tracking how much money we have

--- src/main/java/com/sullbrothers/crypto/mancer/DecisionInterface.java
+++ src/main/java/com/sullbrothers/crypto/mancer/DecisionInterface.java
@@ -3,6 +3,7 @@ package com.sullbrothers.crypto.mancer;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import com.sullbrothers.crypto.coinbase.ExchangeRate;
 import com.sullbrothers.crypto.database.RateHistoryDAO.RateHistory;
@@ -18,9 +19,74 @@ import weka.core.converters.ArffSaver;
  */
 public class DecisionInterface {
 
+    /** This is the number of rate histories in between a data point and it's future "success" point */
+    public static final int POSITIONS_TO_FUTURE = 24*60;
+
     public DecisionInterface () {
         
     }
+
+    /**
+     * Given a set of historical points, can I tell if performing an action on those points would have been beneficial,
+     * and can I also tell to what extent?
+     * 
+     * First pass at this method will return the action that is predicted
+     */
+    public static String predictResult(MancerState state, int rateHistoryPosition){
+        if(rateHistoryPosition + POSITIONS_TO_FUTURE >= state.historicalRates.size()){
+            return null;
+        }
+        RateHistory future = state.historicalRates.get(rateHistoryPosition + POSITIONS_TO_FUTURE);
+        RateHistory curr = state.historicalRates.get(rateHistoryPosition);
+
+        // Can check my math here, but the change in value for buying one currency with another should be like this:
+        // (currA)*Arate + (currB)*Brate = current
+        // (currA + (Brate/Arate) * delta)*Arate' + (currB - delta)*Brate' = new
+        // taking the first derivative with respect to delta:
+        // ((Brate*Arate')/Arate) - Brate' = dPrice/dt
+        Map<String, Double> actionValues = new HashMap<String, Double>();
+        String[] currencies = state.currencyValues.getCurrencies().toArray(new String[0]);
+        for(int i = 0; i < currencies.length; i++){
+            for(int j = 0; j < currencies.length; j++){
+                if(i == j){
+                    continue;
+                }
+                String buyCurrency = currencies[i];
+                String sellCurrency = currencies[j];
+                if(buyCurrency.equalsIgnoreCase("usd") || sellCurrency.equalsIgnoreCase("usd")){
+                    continue;
+                }
+                double currBuyRate = curr.getRates().getExchangeRateByCurrency(buyCurrency).getPrice();
+                double currSellRate = curr.getRates().getExchangeRateByCurrency(sellCurrency).getPrice();
+                double futureBuyRate = future.getRates().getExchangeRateByCurrency(buyCurrency).getPrice();
+                double futureSellRate = future.getRates().getExchangeRateByCurrency(sellCurrency).getPrice();
+                double dValue = (currSellRate*futureBuyRate/currBuyRate) - futureSellRate;
+                String action = buyCurrency + ":" + sellCurrency;
+                actionValues.put(action, dValue);
+            }
+        }
+
+        // TODO: potentially increase  this to something to reflect the desire
+        // to only trade when we make more than a certain amount
+        double maxDelta = 0;
+        String action = "NONE";
+        for(String k : actionValues.keySet()){
+            if(actionValues.get(k) > maxDelta){
+                maxDelta=actionValues.get(k);
+                action = k;
+            }
+        }
+
+        return action;
+    }
+
+    /**
+     * Given a set of historical points, can I bring in data from even further in history to enrich my data for classification?
+     * First thought for this is to take data from 24 hours prior (roughly 1480 positions in the past) and add that to the data row
+     * and let the classifier determine it's value.  Maybe get points from more than just the last day?  Can try several methods and 
+     * plug them into weka and see what happens
+     */
+    //TODO: come up with method sig here
 
     public static MancerAction shouldPerform(MancerState state){
         // TODO: do something brilliant here
@@ -41,16 +107,23 @@ public class DecisionInterface {
             Attribute a = new Attribute(er.getCurrency() + "_AMOUNT");
             dataVector.add(a);
 
+            System.out.printf("currency:%s, fromCurrency:%s\n", er.getCurrency(), er.getFromCurrency());
             currencies.add(er.getCurrency());
         }
 
         // Add the classification attribute
         ArrayList<String> classifications = new ArrayList<String>();
+        System.out.println("Adding possible classification \"NONE\"");
         classifications.add("NONE");
         int i = 0;
         while(i < currencies.size()){
-            int j = i+1;
+            int j = 0;
             while(j < currencies.size()){
+                if(i == j){
+                    j++;
+                    continue;
+                }
+                System.out.println("Adding possible classification \"" + currencies.get(i) + ":" + currencies.get(j) + "\"");                
                 classifications.add(currencies.get(i) + ":" + currencies.get(j));
                 j++;
             }
@@ -60,12 +133,12 @@ public class DecisionInterface {
         dataVector.add(new Attribute("CLASSIFICATION", classifications));
 
         Instances data = new Instances(relationshipName, dataVector, 10);
-        data.setClassIndex(classifications.size());
+        data.setClassIndex(dataVector.size()-1);
 
 
         // Now populate the dataset
         for(RateHistory rh : state.historicalRates){
-            Instance toAdd = new SparseInstance(classifications.size());
+            Instance toAdd = new SparseInstance(dataVector.size());
             i = 0;
             for(String curr : currencies){
                 // i is currency rate
@@ -74,9 +147,14 @@ public class DecisionInterface {
                 // i + 1 is currency amount                
                 // we're not actually storing this at the moment so I'll have to spoof it for now
                 toAdd.setValue(dataVector.get(i+1), getAmountForCurrency(curr));
+                i = i + 2;
             }
             // Using sparse instance to ignore classification for now
-            // toAdd.setValue(dataVector.get(classifications.size()), "?");
+            String action = predictResult(state, state.historicalRates.indexOf(rh));
+            if(action != null){
+                System.out.println("Setting class attribute to " + action + " for " + rh.toString());
+                toAdd.setValue(dataVector.get(dataVector.size()-1), action);
+            }
             data.add(toAdd);
         }
 
